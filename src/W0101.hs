13e7f77428afcb39d2b2f7bad110a6aee2db1e54
hankysteamroller <hanky.steamroller@gmail.com>
2022-05-10 17:13:51 +0300

    Solution for exercise 1

--- src/W0101.hs
+++ src/W0101.hs
@@ -1,10 +1,22 @@
 module W0101 where
 
+import qualified Data.List as L
 -- | Returns the list of all permutations of the argument
 --
 -- >>> import qualified Data.List as L
 -- >>> L.sort (permutations "abc")
 -- ["abc","acb","bac","bca","cab","cba"]
 --
-permutations :: [a] -> [[a]]
-permutations = error "TODO: implement permutations"
+permutations :: Eq a => [a] -> [[a]]
+permutations [] = [[]]
+permutations xs = do
+    x <- xs
+    let rest = L.delete x xs
+    restP <- permutations rest
+    return $ x : restP
+
+listMonadSum :: Num a => [a] -> [a] -> [a]
+listMonadSum xs ys = do
+    x <- xs
+    y <- ys
+    return $ x + y
